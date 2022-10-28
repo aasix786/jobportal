03e620d77b33606b962a678124cf0771ebeb99cc
Siddiquee Khan <siddique.khan.onism@gmail.com>
2022-10-28 21:19:33 +0500

    stacks and skills

---
+++ app/Http/Controllers/Api/SkillController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\SkillRequest;
+use App\Models\Skill;
+
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+
+    public function index()
+    {
+        $skills = Skill::all();
+        if ($skills)
+        {
+            return response()->json(['success'=>true,'skills'=>$skills]);
+        }else
+        {
+            return response()->json(['success'=>false,'message'=>"Some thing went wrong..."],200);
+
+        }
+    }
+
+    public function createSkill(SkillRequest $skillRequest)
+    {
+        $skill = Skill::create($skillRequest->all());
+        if ($skill)
+        {
+            return response()->json(['success'=>true,'skills'=>$skill->name .' created successfully']);
+        }else
+        {
+            return response()->json(['success'=>false,'message'=>"Some thing went wrong..."],200);
+        }
+    }
+    public function editSkill(Request $request,$id)
+    {
+        dd(11);
+        $skill = Skill::find($id)->firstOrFail();
+        if ($skill)
+        {
+            return response()->json(['success'=>true,'skill'=>$skill]);
+        }else
+        {
+            return response()->json(['success'=>false,'message'=>"Some thing went wrong..."],200);
+        }
+
+    }
+    public function updateSkill(SkillRequest $skillRequest,$id)
+    {
+      $skill = Skill::find($id)->firstOrFail();
+
+        if ($skill)
+        {
+            $skill->update($skillRequest->all());
+            return response()->json(['success'=>true,'skill'=>$skill->name.' updated successfully']);
+        }else
+        {
+            return response()->json(['success'=>false,'message'=>"Some thing went wrong..."],200);
+        }
+    }
+
+    public function deleteSkill(Request $request , $id)
+    {
+        $skill = Skill::find($id)->firstOrFail();
+
+        if ($skill)
+        {
+            $skill->delete();
+            return response()->json(['success'=>true,'skill'=>$skill->name.' updated successfully']);
+        }else
+        {
+            return response()->json(['success'=>false,'message'=>"Some thing went wrong..."],200);
+        }
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\PasswordController;
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\DeveloperController;
+use App\Http\Controllers\Api\StackController;
+use App\Http\Controllers\Api\SkillController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -38,5 +40,18 @@ Route::group(['middleware' => 'auth:api'], function () {
     // Developer Routes
     Route::get('/edit_developer', [DeveloperController::class, 'editDeveloper']);
     Route::post('/update_developer/{id}', [DeveloperController::class, 'updateDeveloper']);
+    // Stack Routes
+    Route::get('/stacks', [StackController::class, 'index']);
+    Route::post('/create/stack', [StackController::class, 'createStack']);
+    Route::get('/edit/stack/{id}', [StackController::class, 'editStack']);
+    Route::post('/update/stack/{id}', [StackController::class, 'updateStack']);
+    Route::post('/delete/stack/{id}', [StackController::class, 'deleteStack']);
+    // Skills Routes
+    Route::get('/skills', [SkillController::class, 'index']);
+    Route::post('/create/skill', [SkillController::class, 'createSkill']);
+    Route::get('/edit/skill/{id}', [SkillController::class, 'editSkill']);
+    Route::post('/update/skill/{id}', [SkillController::class, 'updateSkill']);
+    Route::post('/delete/skill/{id}', [SkillController::class, 'deleteSkill']);
+
 
 });
